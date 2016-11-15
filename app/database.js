4a6abef11c3ad137ac08b48405108f99265c2412
Tom Peck <tapeck@umass.edu>
2016-11-15 10:38:53 -0500

    i made it a link at the bottom it seems a little better looking

--- app/database.js
+++ app/database.js
@@ -23,7 +23,7 @@ var initialData = {
       "futurePrice": "4.99",
       "steamLink": 1
     },
-     {
+    {
       "_id": 1,
       "title": "Fake Game 1",
       "beforePrice": "4.99",
@@ -133,76 +133,76 @@ if (data === null) {
  * A dumb cloning routing. Serializes a JSON object as a string, then
  * deserializes it.
  */
-  function JSONClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-  }
+function JSONClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
 
 /**
  * Emulates reading a "document" from a NoSQL database.
  * Doesn't do any tricky document joins, as we will cover that in the latter
  * half of the course. :)
  */
-  export function readDocument(collection, id) {
-// Clone the data. We do this to model a database, where you receive a
-// *copy* of an object and not the object itself.
-    return JSONClone(data[collection][id]);
-  }
+export function readDocument(collection, id) {
+  // Clone the data. We do this to model a database, where you receive a
+  // *copy* of an object and not the object itself.
+  return JSONClone(data[collection][id]);
+}
 
-  export function readEntireDocument(collection) {
-// Clone the data. We do this to model a database, where you receive a
-// *copy* of an object and not the object itself.
-    return JSONClone(data[collection]);
-  }
+export function readEntireDocument(collection) {
+  // Clone the data. We do this to model a database, where you receive a
+  // *copy* of an object and not the object itself.
+  return JSONClone(data[collection]);
+}
 
 /**
  * Emulates writing a "document" to a NoSQL database.
  */
-  export function writeDocument(collection, changedDocument) {
-    var id = changedDocument._id;
-// Store a copy of the object into the database. Models a database's behavior.
-    data[collection][id] = JSONClone(changedDocument);
-// Update our 'database'.
-    localStorage.setItem(startupName, JSON.stringify(data));
-  }
+export function writeDocument(collection, changedDocument) {
+  var id = changedDocument._id;
+  // Store a copy of the object into the database. Models a database's behavior.
+  data[collection][id] = JSONClone(changedDocument);
+  // Update our 'database'.
+  localStorage.setItem(startupName, JSON.stringify(data));
+}
 
 /**
  * Adds a new document to the NoSQL database.
  */
-  export function addDocument(collectionName, newDoc) {
-    var collection = data[collectionName];
-    var nextId = Object.keys(collection).length;
-    while (collection[nextId]) {
-      nextId++;
-    }
-    newDoc._id = nextId;
-    writeDocument(collectionName, newDoc);
-    return newDoc;
+export function addDocument(collectionName, newDoc) {
+  var collection = data[collectionName];
+  var nextId = Object.keys(collection).length;
+  while (collection[nextId]) {
+    nextId++;
   }
+  newDoc._id = nextId;
+  writeDocument(collectionName, newDoc);
+  return newDoc;
+}
 
 /**
  * Reset our browser-local database.
  */
-  export function resetDatabase() {
-    localStorage.setItem(startupName, JSON.stringify(initialData));
-    data = JSONClone(initialData);
-  }
+export function resetDatabase() {
+  localStorage.setItem(startupName, JSON.stringify(initialData));
+  data = JSONClone(initialData);
+}
 
 /**
  * Reset database button.
  */
-  class ResetDatabase extends React.Component {
-    render() {
-      return (
-        <button className="btn btn-default" type="button" onClick={() => {
-          resetDatabase();
-          window.alert("Database reset! Refreshing the page now...");
-          document.location.reload(false);
-        }}>Reset Mock DB</button>
-      );
-    }
+class ResetDatabase extends React.Component {
+  render() {
+    return (
+      <a onClick={() => {
+        resetDatabase();
+        window.alert("Database reset! Refreshing the page now...");
+        document.location.reload(false);
+      }}>Reset Mock DB</a>
+    );
   }
+}
 
-  //ReactDOM.render(
-    //<ResetDatabase />,
-    //document.getElementById('db-reset')
-  //);
+ReactDOM.render(
+  <ResetDatabase />,
+  document.getElementById('db-reset')
+);
