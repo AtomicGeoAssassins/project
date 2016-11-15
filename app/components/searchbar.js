bbdffa44bc961d85d4a2e245e1eb7432361e48f5
Theo Proulx <theoproulx@gmail.com>
2016-11-14 22:36:14 -0500

    attempting to save search variable

--- app/components/searchbar.js
+++ app/components/searchbar.js
@@ -8,17 +8,25 @@ export default class Searchbar extends React.Component {
     };
   }
 
+  getInitialState(){
+    return{searchTerm: ''}
+  }
+
   render() {
     return (
       <form className="navbar-form" role="search">
         <div id="searchbar" className="input-group nav-searched-curved">
-          <input type="text" className="form-control" placeholder="Search" name="srch-term" id="srch-term">
+          <input type="text" className="form-control" onChange={this.searchUpdated} placeholder="Search" name="srch-term" id="srch-term">
             <div className="input-group-btn">
-              <button className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i></button>
+              <a href="searchresults.js" className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i></a>
             </div>
           </input>
         </div>
       </form>
     )
+  }
+
+  searchUpdated(term){
+    this.setState({searchTerm: term})
   }
 }
