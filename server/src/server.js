ebbb44392f6ead16aa62911a9a76cea9d0fccb9c
Tom Peck <tapeck@umass.edu>
2016-11-30 17:33:50 -0500

    working on setting up the structure here

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,36 @@
-// Implement your server in this file.
-// We should be able to run your server with node src/server.js
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+//var util = require("./util.js");
+var bodyParser = require('body-parser');
+var readDocument = require('./database.js').readDocument;
+//var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var validate = require('express-jsonschema').validate;
+var database = require('./database.js');
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
+
+/**
+ * Translate JSON Schema Validation failures into error 400s.
+ */
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
