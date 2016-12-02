19ad1c73042abcd8ffad64c930b6743c14c09a9d
Tom Peck <tapeck@umass.edu>
2016-12-02 01:18:52 -0500

    games page uses gamestable, much simpler now

--- client/app/components/games.js
+++ client/app/components/games.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {getPopularGameData} from '../server';
 import {setActiveNavLink} from '../server';
+import GamesTable from './gamesTable';
 export default class Games extends React.Component {
   constructor(props) {
     super(props);
@@ -19,45 +20,11 @@ export default class Games extends React.Component {
     });
   }
   render(){
-    var Games;
-
-    var adjustPrice = (price) => {
-      if(price == null || price == 0) return "Free!";
-      price = price.toString();
-      return "$" + price.slice(0,price.length-2) + "." + price.slice(price.length-2,price.length);
-    }
-
-    if(this.state.games) {
-      Games = (
-        this.state.games.map((game,i) => {
-          return (
-            <tr key={"line"+i}>
-              <td>{game.name}</td>
-              <td>{adjustPrice(game.original_price)}</td>
-              <td>{adjustPrice(game.final_price)}</td>
-              <td>{adjustPrice(game.future_price)}</td>
-            </tr>
-          )
-        })
-      );
-    }
     return(
       <div>
         <div>
           <h2>List of Games</h2>
-          <table className="table table-hover">
-            <thead className="baby-blue-header">
-              <tr>
-                <th>Game</th>
-                <th>Tomorrow's Price</th>
-                <th>Yesterday's Price</th>
-                <th>Last Month's Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Games}
-            </tbody>
-          </table>
+          <GamesTable games={this.state.games} />
         </div>
       </div>
     );
