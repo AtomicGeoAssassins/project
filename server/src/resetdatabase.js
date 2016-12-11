7565eed45e77e589011af93905490af40a91309c
Tom Peck <tapeck@umass.edu>
2016-12-11 14:29:37 -0500

    reset database action

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -1,7 +1,7 @@
 var ObjectID = require('mongodb').ObjectID;
 
 // Put your startup's name here (only letters and numbers -- no spaces, apostrophes, or special characters!)
-var databaseName = null;
+var databaseName = "db";
 // Put the initial mock objects here.
 var initialData = {
   "boards": [
